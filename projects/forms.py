24b29da57caeec0f036bd23a157d5edb7d7cedfc
Pablo Gonzalez <pablo7.24.93@gmail.com>
2020-06-19 20:42:15 -0500

    add new project

--- projects/forms.py
+++ projects/forms.py
@@ -5,7 +5,7 @@ from projects.models import Owner, Project
 class ProjectForm(forms.Form):
     architect = forms.ModelChoiceField(queryset=Architect.objects.all(), to_field_name='architect__username')
     owner = forms.ModelChoiceField(queryset=Owner.objects.all(), to_field_name='first_name')
-    name = forms.CharField(max_length=255, required=True)
+    name = forms.CharField(max_length=50, required=True)
     area = forms.DecimalField(max_digits=6, decimal_places=2, required=False)
     type = forms.ChoiceField(choices=Project.BUILDING_TYPES)
     start_date = forms.DateField(required=False)
--- projects/views.py
+++ projects/views.py
@@ -8,40 +8,79 @@ from projects.forms import ProjectForm
 
 @login_required
 def list_projects(request):
-    Projects = Project.objects.annotate(
-        sum_estimated_price=Sum('task__concept__estimated_price'),
-        sum_real_price=Sum('task__concept__real_price')
-    )
+    if request.GET:
+        Projects = Project.objects.annotate(
+            sum_estimated_price=Sum('task__concept__estimated_price'),
+            sum_real_price=Sum('task__concept__real_price')
+        ).filter(name__icontains=request.GET['search'])
+    else:    
+        Projects = Project.objects.annotate(
+            sum_estimated_price=Sum('task__concept__estimated_price'),
+            sum_real_price=Sum('task__concept__real_price')
+        )
+        
     return render(request, 'projects/list.html', {'projects': Projects})
 
 @login_required
 def add_project(request):
-    return render(request, 'projects/add_project.html')
+    architects = Architect.objects.values('architect__username')
+    owners = Owner.objects.values('first_name')
+    
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            
+            project = Project(
+                architect=data['architect'],
+                owner=data['owner'],
+                name=data['name'],
+                area=data['area'],
+                type=data['type'],
+                start_date=data['start_date'],
+                end_date=data['end_date']
+            )
+            project.save()
+            
+            return redirect('list_projects')
+    else:
+        form = ProjectForm()
+
+    return render(
+        request, 
+        'projects/add_project.html',
+        {
+            'form': form, 
+            'architects': architects,
+            'owners': owners,
+            'types': Project.BUILDING_TYPES
+        }
+    )
 
 @login_required
 def edit_project(request, id):
-    projectSave = Project.objects.get(pk=id)
+    project = Project.objects.get(pk=id)
+    architects = Architect.objects.values('architect__username')
+    owners = Owner.objects.values('first_name')
     
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
 
-            projectSave.architect = data['architect']
-            projectSave.owner = data['owner']
-            projectSave.name = data['name']
-            projectSave.area = data['area']
-            projectSave.type = data['type']
-            projectSave.start_date = data['start_date']
-            projectSave.end_date = data['end_date']
-            projectSave.save()
+            project.architect = data['architect']
+            project.owner = data['owner']
+            project.name = data['name']
+            project.area = data['area']
+            project.type = data['type']
+            project.start_date = data['start_date']
+            project.end_date = data['end_date']
+            project.save()
             
             return redirect('list_projects')
     else:
         form = ProjectForm()
-    project = Project.objects.get(pk=id)
-    architects = Architect.objects.values('architect__username')
-    owners = Owner.objects.values('first_name')
+
     return render(
         request, 
         'projects/edit_project.html', 
