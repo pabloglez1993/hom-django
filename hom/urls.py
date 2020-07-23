5767a308ce66d3d6b4ad6c755b7d453ab7c0af9e
Pablo Gonzalez <pablo7.24.93@gmail.com>
2020-07-23 18:05:13 -0500

    Last changes

--- hom/urls.py
+++ hom/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
 ]
--- projects/forms.py
+++ projects/forms.py
@@ -5,7 +5,7 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ('architect', 'owner', 'name', 'area', 'type', 'start_date', 'end_date',)
-
+        
 #voy a agregar una clase TaskForm para seguir tu sintaxis en las views porfavor revisa si esta bien
 
 class TaskForm(forms.ModelForm):
